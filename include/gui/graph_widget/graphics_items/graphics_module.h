a2cd7f3ff17d96f2c50911d5858141729ee52e87
Slowpoke <48594426+Slowpoke42@users.noreply.github.com>
2019-05-20 21:05:00 -0700

    graph view progress

--- include/gui/graph_widget/graphics_items/graphics_module.h
+++ include/gui/graph_widget/graphics_items/graphics_module.h
@@ -9,12 +9,18 @@ class module;
 
 class graphics_module : public graphics_node
 {
+    struct module_pin
+    {
+        QString name;
+        u32 net_id;
+    };
+
 public:
     graphics_module(std::shared_ptr<module> m);
 
 private:
-    QVector<QString> m_input_pins;
-    QVector<QString> m_output_pins;
+    QVector<module_pin> m_input_pins;
+    QVector<module_pin> m_output_pins;
 };
 
 #endif // GRAPHICS_MODULE_H
